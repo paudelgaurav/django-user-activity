4bc6ef88f3633197c860d77db72e7afce10bdfab
Gaurav Paudel <gaurav.paudel@wesionary.team>
2022-05-19 02:04:01 +0545

    fixed typo in signals

--- apps/activity_log/signals.py
+++ apps/activity_log/signals.py
@@ -2,17 +2,17 @@ from django.contrib.auth.signals import user_logged_in, user_login_failed
 from django.dispatch import receiver
 
 from .constants import LOGIN, LOGIN_FAILED
-from .models import LogAction
+from .models import ActivityLog
 from .utils import get_client_ip
 
 
 @receiver(user_logged_in)
 def log_user_login(sender, request, user, **kwargs):
     message = f"{user.full_name} is logged in with ip:{get_client_ip(request)}"
-    LogAction.objects.create(user=user, action_type=LOGIN, remarks=message)
+    ActivityLog.objects.create(actor=user, action_type=LOGIN, remarks=message)
 
 
 @receiver(user_login_failed)
 def log_user_login_failed(sender, credentials, request, **kwargs):
     message = f"Login Attempt Failed for number {credentials.get('phone_number')} with ip: {get_client_ip(request)}"
-    LogAction.objects.create(action_type=LOGIN_FAILED, remarks=message)
+    ActivityLog.objects.create(action_type=LOGIN_FAILED, remarks=message)
