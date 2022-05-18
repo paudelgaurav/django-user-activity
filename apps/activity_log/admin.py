1600294bbcf7d48f891cc2a7c6a98b422d469f3b
Gaurav Paudel <gaurav.paudel@wesionary.team>
2022-05-19 01:41:42 +0545

    registered activity log model in admin site as readonly

--- apps/activity_log/admin.py
+++ apps/activity_log/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import ActivityLog
 
-# Register your models here.
+
+class ActivityLogAdmin(admin.ModelAdmin):
+    model = ActivityLog
+
+    def has_delete_permission(self, request, obj=None) -> bool:
+        return False
+
+    def has_add_permission(self, request, obj=None) -> bool:
+        return False
+
+    def has_change_permission(self, request, obj=None) -> bool:
+        return False
+
+
+admin.site.register(ActivityLog, ActivityLogAdmin)
